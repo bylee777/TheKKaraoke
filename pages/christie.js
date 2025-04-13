0b73eefcbb724d14439140359e30affb9f474cda
bylee777 <leebueyo@gmail.com>
2025-04-13 16:17:37 -0400

    first page1

--- pages/christie.js
+++ pages/christie.js
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 import styles from '../styles';
-import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+import { slideIn, staggerContainer } from '../utils/motion';
 
 const Christie = () => (
   <section
