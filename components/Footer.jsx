6d9465dbbc2f3e6694c9f402205db71305cbc150
bylee777 <leebueyo@gmail.com>
2025-04-22 17:35:54 -0400

    footer fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import styles from '../styles';
-import { footerVariants } from '../utils/motion';
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { footerVariants } from "../utils/motion";
 
 const Footer = () => (
   <motion.footer
@@ -16,7 +16,7 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-        🌙 Your Night Out Starts Here
+          🌙 Your Night Out Starts Here
         </h4>
 
         <a
@@ -34,7 +34,6 @@ const Footer = () => (
               alt="map icon"
               className="w-[50px] h-[50px] object-contain"
             />
-         
           </button>
           <span className="text-white text-[12px] mt-1 opacity-80">
             Tap for Google Maps
